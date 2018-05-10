2b8f904593b6a260bdb9cfc7551e5c6d837aeb18
dedchord <15-10123@usb.ve>
2018-05-09 20:52:15 -0400

    Nueva Organizacion

---
+++ LAB1/cos_Taylor.m
@@ -0,0 +1,7 @@
+%Estudiante Jose Barrera 15-10123 Seccion B
+function cos = cos_Taylor(x,n)
+  cos = 0;
+  for i=1:1:n
+    cos = (cos + ((-1)^i)*((x.^(2*i))/(factorial(2*i))));
+  end
+end
---
+++ LAB1/rcos.m
@@ -0,0 +1,8 @@
+%Estudiante Jose Barrera 15-10123 Seccion B
+function cos = rcos(x,n,contador)
+  if contador == n+1
+    cos = (-1).^n;
+  else
+    cos = (((rcos(x,n,contador+1).*((x).^2))./((2*contador)*((2*contador) - 1))) + (-1)^(contador-1));
+  end
+end
