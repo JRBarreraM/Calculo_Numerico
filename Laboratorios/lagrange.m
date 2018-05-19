e6656410a65bede66db0baeea7b2242c72bd7298
dedchord <15-10123@usb.ve>
2018-05-18 21:10:28 -0400

    Ordenamiento Y Nuevos Archivos

---
+++ Laboratorios/lagrange.m
@@ -0,0 +1,42 @@
+function toro = lagrange(A)
+
+close all
+
+clc
+
+n = length(A);
+
+syms x;
+
+m = 0;
+
+x3 = sym(ones(n,1));
+ 
+for i = drange (1:n)
+    
+    for k = drange (1:n)
+        
+        if (i<k||i>k)
+            
+             L = x3(i).*((x - A(k,1))./ (A(i,1)-A(k,1)));
+             x3(i) = L;
+             
+        end
+        
+    end
+      
+end
+
+for j = drange(1:n)
+    
+    P = x3(j).*A(j,2) + m;
+    
+    m = P;
+    
+end
+
+h = simplify(m);
+
+toro = h;
+
+
