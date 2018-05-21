8a47a10d89700949b84beda731326945b0402845
dedchord <15-10123@usb.ve>
2018-05-20 20:25:05 -0400

    Comentarios

--- LAB2/Lab2.m
+++ LAB2/Lab2.m
@@ -5,9 +5,9 @@ b2 = A2 * [1;-1;1];
 
 disp('---Parte 1---');
 
-% b) Por teorema Rouche-Frobenius, si rg(A) =rg(A*), donde A* es la matriz
+% b) Por teorema Rouche-Frobenius, si rango de A es igual al rango de la matriz
 % ampliada de A con b, y esto igual al número de incógnitas, entonces 
-% tenemos un SEL con una única solución.
+% tenemos que el SEL tiene una única solución.
 y = sprintf('---Ejercicio b---');
 disp(y);
 C1 = [A1 b1];
@@ -68,7 +68,11 @@ disp(obtenido);
 disp('Error relativo Solucion 2');
 disp(errorelativo([1;-1;1],obtenido));
 
-
+% Es sencillo ver que la aplicacion del pivoteo no hace mayor diferencia sobre
+% sobre la matriz A2 pues esta es diagonal dominante. Mientras que en A1 el
+% el primer pivote es un numero muy pequeno y dividir entre el causa errores.
+% De alli la importancia del pivoteo con la aritmetica inexacta, si se usa
+% aritmetica exacta realmente no hay diferencia.
 
 disp('---Ejercicio d---');
 disp('Determinante de A1');
@@ -92,6 +96,14 @@ disp('Se Verifica la Relacion para A2 sin pivote?');
 disp(verificarrelacion(A2,b2,0));
 disp('Se Verifica la Relacion para A2 con pivote?');
 disp(verificarrelacion(A2,b2,1));
+% El determinante de si es cercano a 0, la matriz se hace casi singular para la
+% aritmetica del computador lo cual dificulta el calculo de la inversa y la
+% resolucion de SEL. El numero de condicion entre mas se aleja de 1 influye
+% mas gravemente en la cota del error de salida dado en error en la entrada.
+% Los autovalores de la matriz nos dan informacion relacionada a la singularidad,
+% de la matriz, entre mas cercanos a 0, mas se comprometen los calculos, algo
+% similar a lo ya explicado con el determinante.
+% Pudimos ver que la relacion teorica siempre se verifica.
 
 disp('---Parte 2---');
 for n=2:13
@@ -118,3 +130,12 @@ plot(intervalo,resultados,'-r');
 title('Numero de Condicion vs Tamaño');
 xlabel('Tamaño De La Matriz De Hilbert');
 ylabel('Numero De Condicion De La Matriz');
+
+% Con un n de 13 se puede apreciar un error relativo superior al 100%
+% y con 12 inferior.
+% Como conclusion se puede observar que la matriz para n = 13, poseia un
+% numero de condicion tan grande que aun sin error en la entrada, el algoritmo
+% estudiado poseia un error de mas del 100%. Tambien se observa que el crecimie-
+% pronunciado de la grafica, muy superior al lineal en principio, se dispara
+% para n=13. Por ultimo, vale la pena destacar la sorprende exactitud del algo-
+% ritmo A\b.
